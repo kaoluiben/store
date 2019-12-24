bc125e59541e2de97d9a06d97751b1e993c36144
kaoluiben@gmail.com <kaoluiben@gmail.com>
2019-12-24 09:37:15 +0800

    upload

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -32,6 +32,35 @@ class _LoginPageState extends State<LoginPage> {
     //RaisedButton是有陰影，看起來凸起來的。
     //FloatingActionButton是在側面浮起來的那種按鈕。
     //OutlineButton ，帶邊框的按鈕中文叫線框按鈕。
-    return OutlineButton();
+    return OutlineButton(
+      splashColor: Colors.grey, //點擊按鈕時水波紋的顏色
+      onPressed: () {},
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(40)), //設置按鈕的形狀，半圓角的矩形邊，
+      highlightElevation: 0, //陰影的範圍，值越大陰影範圍越大
+      borderSide: BorderSide(color: Colors.grey), //邊框顏色
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image(
+              image: AssetImage("assets/google_log.png"),
+              height: 35.0,
+            ),
+            Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Text(
+                  "Sign in with Google",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.grey,
+                  ),
+                )),
+          ],
+        ),
+      ), //給子節點設置內邊距屬性
+    );
   }
 }
