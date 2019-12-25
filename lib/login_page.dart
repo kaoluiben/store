a7f236367cf2e4e3d0e883ad79f1f3020e55c948
kaoluiben@gmail.com <kaoluiben@gmail.com>
2019-12-25 10:48:56 +0800

    upload

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -20,7 +20,7 @@ class _LoginPageState extends State<LoginPage> {
             children: <Widget>[
               FlutterLogo(size: 150),
               SizedBox(height: 50),
-              _signInButton(),
+              _googleSignInButton(),
             ],
           ),
         ),
@@ -28,25 +28,24 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  Widget _signInButton() {
+  //Phone SignIn
+
+  //Google SignIn
+  Widget _googleSignInButton() {
     //在flutter的控件裡常用按鈕有：FlatButton,RaisedButton，FloatingActionButton,OutlineButton
     //FlatButton是扁平的，沒有陰影的。
     //RaisedButton是有陰影，看起來凸起來的。
     //FloatingActionButton是在側面浮起來的那種按鈕。
     //OutlineButton ，帶邊框的按鈕中文叫線框按鈕。
     return OutlineButton(
-      splashColor: Colors.grey, //點擊按鈕時水波紋的顏色
-      onPressed: () {
-        signInWithGoogle().whenComplete(() {
-          Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-            return FirstScreen();
-          }));
-        });
-      },
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(40)), //設置按鈕的形狀，半圓角的矩形邊，
-      highlightElevation: 0, //陰影的範圍，值越大陰影範圍越大
-      borderSide: BorderSide(color: Colors.grey), //邊框顏色
+      //點擊按鈕時水波紋的顏色
+      splashColor: Colors.grey,
+      //設置按鈕的形狀，半圓角的矩形邊
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+      //陰影的範圍，值越大陰影範圍越大
+      highlightElevation: 0,
+      //邊框顏色
+      borderSide: BorderSide(color: Colors.grey),
       //給子節點設置內邊距屬性
       child: Padding(
         padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
@@ -70,6 +69,14 @@ class _LoginPageState extends State<LoginPage> {
           ],
         ),
       ),
+
+      onPressed: () {
+        signInWithGoogle().whenComplete(() {
+          Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+            return FirstScreen();
+          }));
+        });
+      },
     );
   }
 }
